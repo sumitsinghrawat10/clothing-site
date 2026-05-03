ffa0885eab46dc321a9df7e1053c6032334a9efd
sumit.singh <sumitsingh1006@gmail.com>
2026-05-03 16:56:47 +0530

    first commit

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,6 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ['**/*.jpej'],
   server: {
     port: 3000,   //  now runs on localhost:3000
   },
